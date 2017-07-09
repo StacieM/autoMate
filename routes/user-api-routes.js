e29b3021aa2b33415815471ed5da9a32c25a7888
cmontoya13 <chris@chrismontoya.com>
2017-07-09 12:06:28 -0700

    working server

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -3,18 +3,18 @@ var db = require("../models");
 module.exports = function (app) {
 
     //do i need app.get?
-    // app.get("/api/newUser", function (req, res) {
-    //     db.User.findAll({}).then(function (dbUser) {
-    //         res.json(dbUser);
-    //     });
-    // });
+    app.get("/api/newUser", function (req, res) {
+        db.User.findAll({}).then(function (dbUser) {
+            res.json(dbUser);
+        });
+    });
 
     app.post("/api/newUser", function (req, res) {
         db.User.create({
             firstName: req.body.firstName,
             lastName: req.body.lastName,
-            address: req.body, address,
-            city: req.body.address,
+            address: req.body.address,
+            city: req.body.city,
             state: req.body.state,
             zip: req.body.zip,
             phone: req.body.phone,
