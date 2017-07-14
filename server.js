64a9e806e118614f6598ebb1342d7b6f4a02c781
cmontoya13 <chris@chrismontoya.com>
2017-07-13 20:28:08 -0700

    working on newUser

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require("express");
+var session = require('express-session');
+var passport = require('passport');
 var bodyParser = require("body-parser");
+var passport = require('passport');
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -11,13 +14,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-
-var db = require("./models");
-
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/html-routes.js")(app);
-require("./routes/user-api-routes.js")(app);
+require("./routes/user-api-routes.js")(app, passport);
 require("./routes/schedule-api-routes.js")(app);
+require('./config/passport.js')(passport, db.User);
 
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
